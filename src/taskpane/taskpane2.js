b4ba346244ea306ab80dfa6744f1dc441e972a66
kelvin-otieno <kelvinotieno06@gmail.com>
2025-05-07 12:50:21 +0300

    added search for regarding and ability to show missing emails

--- src/taskpane/taskpane2.js
+++ src/taskpane/taskpane2.js
@@ -3,6 +3,8 @@
  * See LICENSE in the project root for license information.
  */
 
+const { create } = require("core-js/core/object");
+
 /* global document, Office */
 
 // Office.onReady((info) => {
@@ -12,10 +14,32 @@
 //       document.getElementById("run").onclick = run;
 //     }
 //   });
+
+var regardingItem = null;
+
+////////////STAGING/////////////////////////////////
+// const createemailapi = "https://prod-200.westeurope.logic.azure.com:443/workflows/7fc3dd1d8348461bb773102354791678/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9Wd5IGNofy0lJFr1u0YKqPtifAVjI5d1UxyFGQv14kk";
+// const searchregardingapi = "https://prod-81.westeurope.logic.azure.com:443/workflows/1ddbbdd778ee4104991266039f724f4a/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9jnkdoeMtPv6_ficU6q2RKTxLymJQxnGDGozYUDfZpg";
+// const searchmissingemailsapi = "https://prod-80.westeurope.logic.azure.com:443/workflows/45dcfb9f75d04f1a8ad03f2996ff94e8/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hPy8mfFdYQI_hVDNpJ_DH-vfWj1gygrXTAPWQaiF9U8";
+
+////////////LIVE/////////////////////////////////
+const createemailapi = "https://prod-60.westeurope.logic.azure.com:443/workflows/d176927b3cac453e8f3c41b812655c7e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1nlpUWi5lOdE2k9iFTx7-FTdgQooyPvmYgybyiAqNs0";
+const searchregardingapi = "https://prod-150.westeurope.logic.azure.com:443/workflows/fb5e125f2eb640bf8aba86b15b9aeb03/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=GqpQnsIgYp_2hL1bz2S9EpUpxr60qQyfQSdttetQQLI";
+const searchmissingemailsapi = "https://prod-73.westeurope.logic.azure.com:443/workflows/37d8b1ec35454bfcbc5ca129c06823af/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jUMDas16sL2BarEnMn4WOkP-MayqLNpJY5_-skRw1tQ";
   
 function loadControls() {
+  loadMissingEmails();
+  document.getElementById("searchBox").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent default form submission
+        const value = event.target.value;
+        showSuggestionsOnEnter(value); // Call the function
+    }
+  });
   Office.onReady((info) => {
     if (info.host === Office.HostType.Outlook) {
+
+     
           // Your code here
     const item = Office.context.mailbox.item;
     const dateTimeCreated = item.dateTimeCreated;
@@ -35,6 +59,52 @@ function loadControls() {
     }
   });
 
+
+
+}
+
+function loadMissingEmails() {
+  Office.onReady((info) => {
+    if (info.host === Office.HostType.Outlook) {
+      document.getElementById("run").innerHTML = "Tracking.....";
+      const item = Office.context.mailbox.item;
+          
+      const missingemailstext = document.getElementById("missingemailstext");
+        
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      const from = item.from;
+      const to = item.to;
+      const cc = item.cc;
+      const raw = JSON.stringify({
+        from: from,
+        to: to,
+        cc: cc
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(
+        searchmissingemailsapi,
+        requestOptions
+      )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.missingemails.length > 0) {
+          const missingemailstitle = document.getElementById("missingemailstitle");
+          missingemailstitle.style.display = "block";
+          missingemailstext.innerText = result.missingemails;
+        }
+        
+      })
+      .catch((error) => console.error(error));
+    }
+  });
 }
 
  async function run() {
@@ -95,7 +165,7 @@ function loadControls() {
         };
   
         fetch(
-          "https://prod-60.westeurope.logic.azure.com:443/workflows/d176927b3cac453e8f3c41b812655c7e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1nlpUWi5lOdE2k9iFTx7-FTdgQooyPvmYgybyiAqNs0",
+          createemailapi,
           requestOptions
         )
           .then((response) => {
@@ -164,3 +234,115 @@ function loadControls() {
   
     return `${utcYear}-${utcMonth}-${utcDay}T${utcHours}:${utcMinutes}:${utcSeconds}`;
   }
+
+  // const suggestionsList = ["Apple","Almond","Asbestos", "Banana", "Cherry", "Date", "Grapes", "Mango", "Orange", "Pineapple", "Strawberry"];
+  const suggestionsList = [];
+
+function showSuggestions(value) {
+    const suggestionsDiv = document.getElementById("suggestions");
+    suggestionsDiv.innerHTML = "";
+    if (value.length === 0) {
+        suggestionsDiv.style.display = "none";
+        return;
+    }
+    
+    const filteredSuggestions = suggestionsList.filter(item => item.toLowerCase().startsWith(value.toLowerCase()));
+    
+    if (filteredSuggestions.length > 0) {
+        suggestionsDiv.style.display = "block";
+        filteredSuggestions.forEach(suggestion => {
+            const div = document.createElement("div");
+            div.classList.add("suggestion-item");
+            div.innerText = suggestion;
+            div.onclick = () => {
+                document.getElementById("searchBox").value = suggestion;
+                suggestionsDiv.style.display = "none";
+            };
+            suggestionsDiv.appendChild(div);
+        });
+    } else {
+        suggestionsDiv.style.display = "none";
+    }
+}
+
+function clearSuggestions(value) {
+  const suggestionsDiv = document.getElementById("suggestions");
+  suggestionsDiv.innerHTML = "";
+  suggestionsDiv.style.display = "none";
+  if (value.length === 0) {
+      // suggestionsDiv.style.display = "none";
+      regardingItem = null;
+      console.log("regardingItem: ", regardingItem);
+  }
+}
+
+function showSuggestionsOnEnter(value) {
+  const suggestionsDiv = document.getElementById("suggestions");
+  const searchText = document.getElementById("searchText");
+  searchText.style.display = "block";
+  //show loading animation
+  // const divload = document.createElement("div");
+  // divload.classList.add("suggestion-item");
+  // divload.innerText = "searching...";
+  // suggestionsDiv.appendChild(divload);
+  // suggestionsDiv.style.display = "block";
+
+  const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "input": value
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(searchregardingapi, requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+    searchText.style.display = "none";
+    
+    suggestionsDiv.innerHTML = "";
+    if (result.length === 0) {
+        suggestionsDiv.style.display = "none";
+        return;
+    }
+    else{
+        suggestionsList.length = 0; // Clear the array
+        result.forEach(item => {
+            suggestionsList.push(item);
+        });
+    }
+    
+    // const filteredSuggestions = suggestionsList.filter(item => item.toLowerCase().startsWith(value.toLowerCase()));
+    const filteredSuggestions = suggestionsList;
+    
+    if (filteredSuggestions.length > 0) {
+        suggestionsDiv.style.display = "block";
+        filteredSuggestions.forEach(suggestion => {
+            const div = document.createElement("div");
+            div.classList.add("suggestion-item");
+            div.innerText = suggestion.name + " (" + suggestion.recordtype + ")";
+            div.onclick = () => {
+                document.getElementById("searchBox").value = suggestion.name + " (" + suggestion.recordtype + ")";
+                regardingItem = suggestion;
+                suggestionsDiv.style.display = "none";
+                console.log("regardingItem: ", regardingItem);
+            };
+            suggestionsDiv.appendChild(div);
+        });
+    } else {
+        suggestionsDiv.style.display = "none";
+    }
+
+  })
+  .catch((error) => console.error(error));
+
+
+}
+
+
