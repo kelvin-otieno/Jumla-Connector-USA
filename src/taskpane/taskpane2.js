493c5465b865577c4a354518061735105a6b742a
kelvin-otieno <kelvinotieno06@gmail.com>
2025-12-04 16:13:44 +0300

    use variable for createemail fetch request

--- src/taskpane/taskpane2.js
+++ src/taskpane/taskpane2.js
@@ -209,7 +209,7 @@ function loadMissingEmails() {
           regardingopp: regardingItemOpp
         });
 
-        createemailapi = "https://a26068ef5a2445e0ad4ddab310c157.f9.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/d176927b3cac453e8f3c41b812655c7e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FSyhr-Ow7020W530ouX_9abfO8ry8Et-weh4zQ9BYVI";
+        
   
         const requestOptions = {
           method: "POST",
@@ -217,9 +217,11 @@ function loadMissingEmails() {
           body: raw,
           redirect: "follow",
         };
+
+        createemailapi = "https://a26068ef5a2445e0ad4ddab310c157.f9.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/d176927b3cac453e8f3c41b812655c7e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FSyhr-Ow7020W530ouX_9abfO8ry8Et-weh4zQ9BYVI";
   
         fetch(
-          "https://a26068ef5a2445e0ad4ddab310c157.f9.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/d176927b3cac453e8f3c41b812655c7e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FSyhr-Ow7020W530ouX_9abfO8ry8Et-weh4zQ9BYVI",
+          createemailapi,
           requestOptions
         )
           .then((response) => {
